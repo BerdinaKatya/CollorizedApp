89687c2e34b3e6cdaa7bdb0e45d51b8587e3f9a8
BerdinaKatya <kuznkate42@gmail.com>
2023-07-28 13:13:49 +0500

    Initial Commit

---
+++ CollorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollorizedApp
+//
+//  Created by Екатерина Кузнецова on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
