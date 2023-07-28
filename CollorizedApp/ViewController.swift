ef7cb540ec852cc6ede938a2834041e250c300de
BerdinaKatya <kuznkate42@gmail.com>
2023-07-28 17:41:21 +0500

    Setup app logic

--- CollorizedApp/ViewController.swift
+++ CollorizedApp/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
     }
 
 
+    @IBAction func redSliderAction() {
+        let roundedValue = round(redSlider.value * 100) / 100
+        redValueLabel.text = roundedValue.formatted()
+        changeSliderColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        let roundedValue = round(greenSlider.value * 100) / 100
+        greenValueLabel.text = roundedValue.formatted()
+        changeSliderColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        let roundedValue = round(blueSlider.value * 100) / 100
+        blueValueLabel.text = roundedValue.formatted()
+        changeSliderColor()
+    }
+    
+    private func changeSliderColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
-
